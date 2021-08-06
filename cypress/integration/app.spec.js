0590fc060c2c3c0175ce627dca01012d9006b74b
Colin <cln.gardiner@outlook.com>
2021-08-06 15:13:12 +0100

    added designs

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -57,3 +57,30 @@ describe('bookmark manager index page', () => {
         cy.get('#url1').should('not.exist')
     })
 });
+
+describe('commenting on bookmarks', () => {
+    
+    beforeEach(() => {
+        cy.task('taskTruncateTables');
+        cy.task('taskCreateTestBookmark');
+    })
+
+    it('commenting on a bookmark', () => {
+        cy.visit('/');
+        cy.get('#comment-box-1').type('for the cats')
+        cy.get('#comment-button-1').click()
+        cy.get('#bookmark-1-comment-1').should('contain', 'for the cats')
+    })
+
+    it('commenting on the correct bookmark', () => {
+        cy.visit('/');
+        cy.get('#url-box').type('www.reddit.com')
+        cy.get('#tag-box').type('procrastination')
+        cy.get('#add-button').click()
+        cy.get('#comment-box-2').type('I love reddit memes!')
+        cy.get('#comment-button-2').click()
+        cy.get('#bookmark-2-comment-1').should('contain', 'I love reddit memes!')
+    })
+
+});
+
